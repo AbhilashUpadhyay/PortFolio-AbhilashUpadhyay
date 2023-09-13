2eb6f500482a46bc301f5ccf5d0f5f31610daf23
Abhilash Upadhyay <93393330+AbhilashUpadhyay@users.noreply.github.com>
2023-09-13 16:11:52 +0530

    Add files via upload

---
+++ portfolio/app.js
@@ -0,0 +1,9 @@
+
+let About =document.getElementById('about').addEventListener('click',()=>{
+    window.scrollTo(0,600)
+    
+})
+let Project =document.getElementById('project').addEventListener('click',()=>{
+    window.scrollTo(0,1200)
+    
+})
