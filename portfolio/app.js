9bf40603eb48bacc8b88dfa356d74260bad7c553
Abhilash Upadhyay <93393330+AbhilashUpadhyay@users.noreply.github.com>
2023-09-15 13:03:01 +0530

    Delete portfolio directory

--- portfolio/app.js
+++
@@ -1,9 +0,0 @@
-
-let About =document.getElementById('about').addEventListener('click',()=>{
-    window.scrollTo(0,600)
-    
-})
-let Project =document.getElementById('project').addEventListener('click',()=>{
-    window.scrollTo(0,1200)
-    
-})
