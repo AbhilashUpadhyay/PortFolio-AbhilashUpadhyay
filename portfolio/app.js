387a7d04bba878e72d69383030f5948759ef94d3
Abhilash Upadhyay <93393330+AbhilashUpadhyay@users.noreply.github.com>
2023-09-25 08:31:42 -0300

    Add files via upload

---
+++ portfolio/app.js
@@ -0,0 +1,17 @@
+
+let About =document.getElementById('about').addEventListener('click',()=>{
+    window.scrollTo(0,600)
+    
+})
+let Experience =document.getElementById('experience').addEventListener('click',()=>{
+    window.scrollTo(0,1150)
+    
+})
+let Project = document.getElementById('project').addEventListener('click',()=>{
+    window.scrollTo(0,1750)
+    
+})
+let Contact = document.getElementById('contact').addEventListener('click',()=>{
+    window.scrollTo(0,2350)
+    
+})
